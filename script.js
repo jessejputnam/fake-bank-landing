457ce90a0b42696569f6aabf6d6a942f80bc7b6a
Jesse Putnam <66268183+jessejputnam@users.noreply.github.com>
2021-12-10 14:02:35 -0500

    Style desktop footer; add bkgrd blur mobile nav

--- script.js
+++ script.js
@@ -5,6 +5,13 @@ const hamburgerTop = document.querySelector(".hamburger__top");
 const hamburgerMiddle = document.querySelector(".hamburger__middle");
 const hamburgerBottom = document.querySelector(".hamburger__bottom");
 const mobileMenu = document.querySelector(".navmenu--mobile");
+const header = document.querySelector(".header__banner");
+const hero = document.querySelector(".hero");
+const intro = document.querySelector(".intro__info");
+const footer = document.querySelector(".footer__container");
+const services = document.querySelector(".services");
+const articles = document.querySelector(".articles");
+const main = document.querySelector(".main__container");
 
 hamburger.addEventListener("click", function () {
   hamburgerTop.classList.toggle("hamburger__top--open");
@@ -13,7 +20,17 @@ hamburger.addEventListener("click", function () {
 
   if (!hamburgerMiddle.classList.contains("hamburger__middle--open")) {
     mobileMenu.classList.add("hidden-menu");
+    intro.classList.remove("background");
+    services.classList.remove("background");
+    articles.classList.remove("background");
+    footer.classList.remove("background");
+    hero.classList.remove("background");
   } else {
     mobileMenu.classList.remove("hidden-menu");
+    intro.classList.add("background");
+    services.classList.add("background");
+    articles.classList.add("background");
+    footer.classList.add("background");
+    hero.classList.add("background");
   }
 });
